90125a99e45cdb4f992d3141293c77c5c4c252a7
Tismagic <duanrancosmos@gmail.com>
2022-04-08 18:02:12 +0800

    first commit

---
+++ src/main/java/com/example/yilimusic/controller/DefaultController.java
@@ -0,0 +1,18 @@
+package com.example.yilimusic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping
+    public String sayHello(){
+        return "欢迎duanran music";
+    }
+
+
+
+}
